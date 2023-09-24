3f6db577217ec2224ea47b1a8342d53649794bbb
Shawna-lee1 <104038577+Shawna-lee1@users.noreply.github.com>
2023-09-23 21:26:14 -0400

    Update server-s.py
    server-s code

--- server-s.py
+++ server-s.py
@@ -1,6 +1,59 @@
 #!/usr/bin/env python3
 
 import sys
+import socket
+import struct
 
-if __name__ == '__main__':
-    sys.stderr.write("server is not implemented yet\n")
+# Constants from the specification
+MAX_UDP_PACKET_SIZE = 424
+INITIAL_SEQUENCE_NUMBER = 50000
+
+# Function to create the Confundo header
+def create_header(seq_num, ack_num, conn_id, ack_flag=False, syn_flag=False, fin_flag=False):
+    flags = 0
+    if ack_flag:
+        flags |= 0x10
+    if syn_flag:
+        flags |= 0x02
+    if fin_flag:
+        flags |= 0x01
+    header = struct.pack('!IIBB', seq_num, ack_num, conn_id, flags)
+    return header
+
+# Function to send a packet
+def send_packet(sock, data, seq_num, ack_num, conn_id, ack_flag=False, syn_flag=False, fin_flag=False):
+    header = create_header(seq_num, ack_num, conn_id, ack_flag, syn_flag, fin_flag)
+    packet = header + data.encode('utf-8')
+    sock.sendto(packet, client_address)
+
+# Parse command-line arguments
+if len(sys.argv) != 2:
+    sys.stderr.write("ERROR: Invalid number of arguments\n")
+    sys.exit(1)
+
+server_port = int(sys.argv[1])
+
+# Create a UDP socket
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+server_socket.bind(('0.0.0.0', server_port))
+
+# Listen for incoming connections and respond
+while True:
+    data, client_address = server_socket.recvfrom(MAX_UDP_PACKET_SIZE)
+    header = struct.unpack('!IIBB', data[:12])
+    seq_num, ack_num, conn_id, flags = header
+    print(f"RECV {seq_num} {ack_num} {conn_id} {'ACK' if flags & 0x10 else ''} {'SYN' if flags & 0x02 else ''} {'FIN' if flags & 0x01 else ''}")
+
+    if flags & 0x02:  # If SYN flag is set, respond with SYN-ACK
+        conn_id += 1
+        ack_num = seq_num + 1
+        seq_num = INITIAL_SEQUENCE_NUMBER
+        send_packet(server_socket, '', seq_num, ack_num, conn_id, ack_flag=True, syn_flag=True)
+
+    # Handle other packet types (ACK, FIN) as needed
+
+    # Send data or ACK packets as needed
+
+# Close the server socket (this code may never be reached)
+server_socket.close()
+
